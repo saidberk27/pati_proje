8b6db609f2ea48b3f330303dbb626431c682d28e
cesebe27 <csaidberk@gmail.com>
2019-03-19 18:33:28 +0300

    mama kabi ui dosyasi eklendi

--- asistan/as.py
+++
@@ -1,5 +0,0 @@
-import time
-arduino = open("/dev/ttyUSB1", "r")
-while True:
-    print(arduino.read())
-    time.sleep(1.5)
---
+++ asistan/main.py
@@ -0,0 +1,4 @@
+ondalik_r = list(range(1,10))
+birlik_r = list(range(10))
+
+print(ondalik_r,birlik_r)
