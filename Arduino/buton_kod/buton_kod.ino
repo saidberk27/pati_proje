8f8c06e9946fc9142debea79e4b00a2e3b422825
cesebe27 <csaidberk@gmail.com>
2019-03-20 16:47:35 +0300

    Mama Kabi Kodu Mama Deposu Koduyla Birleştirildi Arduino Dizinine EKlendi Dizinlerde Değişiklik Yapıldı

--- Arduino/buton_kod/buton_kod.ino
+++ Arduino/buton_kod/buton_kod.ino
@@ -1,5 +1,10 @@
+#include <Servo.h>
+Servo mama_depo;
+
+
 void setup(){
   pinMode(13,INPUT);
+  mama_depo.attach(9);
   Serial.begin(9600);
 }
 
@@ -7,11 +12,13 @@ void loop(){
   if(digitalRead(13) == HIGH){
     Serial.println("Kap Bos");
     delay(1000);
+    mama_depo.write(110);
   }
   
   else{
     Serial.println("Kap Dolu");
     delay(1000);
+    mama_depo.write(0);
   }
   
 }
