c18a240235a60822e983ab4d5249b7afdffb6754
cesebe27 <csaidberk@gmail.com>
2019-03-27 15:57:33 +0300

    Dizinler Düzenlendi.GUI Ana Sayfası Tamamen Bitti

--- Dosya_Okuma/dosya_okuma.py
+++
@@ -1,4 +0,0 @@
-import time
-f= open("/dev/ttyUSB0","r")
-csb = f.readlines()
-print(str(f.readlines()))
--- GUI/asistan/main.py
+++
@@ -1,4 +0,0 @@
-ondalik_r = list(range(1,10))
-birlik_r = list(range(10))
-
-print(ondalik_r,birlik_r)
