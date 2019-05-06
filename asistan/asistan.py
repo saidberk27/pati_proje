79751156f0fae07c3e061113c978e530f10909ca
csb27 <38182497+csbtwentyseven@users.noreply.github.com>
2019-05-06 15:14:46 +0300

    Ses Dosyaları Eklendi

--- asistan/asistan.py
+++ asistan/asistan.py
@@ -1,17 +1,42 @@
 import speech_recognition as sr
 import playsound
+import time
+import sys
 r = sr.Recognizer()
+csb = "Hava Sicakliği,27,santigrat derece"
+
+def sicaklik_soyle():
+    print("Sıcaklık")
+    playsound.playsound('C:/Users/BERKGILLER/Desktop/asistan/hava_sicakligi.mp3', True)
+    playsound.playsound('C:/Users/BERKGILLER/Desktop/asistan/yirmi.mp3', True)
+    playsound.playsound('C:/Users/BERKGILLER/Desktop/asistan/yedi.mp3', True)
+    playsound.playsound('C:/Users/BERKGILLER/Desktop/asistan/santigrat.mp3', True)
 while True:
     with sr.Microphone() as source:
-        print("Bi Şeyler De")
+        print("Ne Söylemek İsterdiniz?")
         audio = r.listen(source)
 
     try:
-        text = r.recognize_google(audio, language='tr-tr')
-        print(text,"Dedin")
-        if(text == "Merhaba"):
-            playsound.playsound('C:/Users/BERKGILLER/Desktop/merhaba.mp3', True)
+        komut = r.recognize_google(audio, language='tr-tr')
+        print(komut)
+        if(komut == "Merhaba"):
+            playsound.playsound('C:/Users/BERKGILLER/Desktop/asistan/merhaba.mp3', True)
 
-    except:
-        print("Try Again")
+        elif(komut == "kendinden bahset"):
+            print("ben zicey")
+            playsound.playsound('C:/Users/BERKGILLER/Desktop/asistan/zicey.mp3', True)
+        elif(komut == "su deposunun durumu nedir"):
+            print("bu da su deposu")
+        elif(komut == "mama deposunun durumu nedir"):
+            print("mama deposu")
+        elif(komut == "kum deposunun durumu nedir"):
+            print("kum deposu")
+        elif(komut == "Sıcaklık nedir"):
+            sicaklik_soyle()
+        elif(komut == "çıkış yap"):
+            print("çkş")
+            sys.exit()
+
+    except FileNotFoundError:
+        print("Lütfen Tekrar Deneyiniz")
 
